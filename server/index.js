3f1674b67465640f133d62ee055e909dc03e8370
IATIA224 <jorvincesoriano3@gmail.com>
2025-12-02 13:10:32 +0800

    try to fix email

--- server/index.js
+++ server/index.js
@@ -4,13 +4,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const app = express();
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://lakbai-xx0.onrender.com', // Your deployed frontend URL
-  ],
-  credentials: true
-}));
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
